6a94319815fd6c781ad6da411cf7dfae5ed2391f
Vladislav <mistergeralt@gmail.com>
2023-06-01 17:14:40 +0300

    step 3: added Makefile + picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested;
+
+    @Option(names = {"-v", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
